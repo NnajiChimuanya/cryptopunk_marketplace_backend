6ff7c0f8b08373b07fd20ec5bbcc5d07f14486f5
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-07-05 10:21:11 +0400

    debugging

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello");
 });
 
-app.get("/getNfts", async (req, res) => {
-  let data = await axios.get(
-    "https://testnets-api.opensea.io/assets?asset_contract_address=0xE3320a32f3c5959845C6A39b3B0d29D869A8F968&order_direction=asc"
-  );
-  res.status(200).json(data.data.assets);
-});
+// app.get("/getNfts", async (req, res) => {
+//   let data = await axios.get(
+//     "https://testnets-api.opensea.io/assets?asset_contract_address=0xE3320a32f3c5959845C6A39b3B0d29D869A8F968&order_direction=asc"
+//   );
+//   res.status(200).json(data.data.assets);
+// });
 
 app.listen(port, () => console.log(`listening at port : ${port}`));
