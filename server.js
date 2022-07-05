8f2534bc1b7267de9683baaf147c53bce9c62832
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-07-05 23:44:56 +0400

    Made alittle change to the order

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 
 app.get("/getNfts", async (req, res) => {
   let data = await axios.get(
-    "https://testnets-api.opensea.io/assets?asset_contract_address=0xE3320a32f3c5959845C6A39b3B0d29D869A8F968&order_direction=desc"
+    "https://testnets-api.opensea.io/assets?asset_contract_address=0xE3320a32f3c5959845C6A39b3B0d29D869A8F968"
   );
   res.status(200).json(data.data.assets);
 });
